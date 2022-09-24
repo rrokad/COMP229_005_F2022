5c03fc40a8e15b2a0d909541e403765cb99d0aa1
rrokad <107492186+rrokad@users.noreply.github.com>
2022-09-24 12:00:52 -0400

    Week 03 Deleted files

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const hello = require('./hello');
 
 hello.sayHello();
@@ -5,3 +6,16 @@ hello.sayHello();
 const lib = require('./lib');
 console.log(lib.halfof(10));
 console.log(lib.multiply(10, 2));
+=======
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+>>>>>>> Stashed changes
